45b12db009603be4c769fabfa3bebe5902807593
Matthew Cassell <mcassell411@gmail.com>
2024-05-26 11:45:48 -0500

    Create boot.s
    BIOS routines

---
+++ src/BIOS/boot.s
@@ -0,0 +1,18 @@
+  org 0x7c00
+
+  mov bx, msg
+  call BIOSprint
+
+  mov dx, 0x4142
+  call HEXprint
+
+hang:
+  jmp hang
+
+  msg: db 'Something OS', 13, 10, 0
+
+  times 510-($-$$) db 0
+  dw 0xaa55
+
+%include "BIOS/biosprint.s"
+%include "BIOS/hexprint.s"
