e01bdbea9f25a16a112eccdad6527c266f086aaf
Matthew Cassell <mcassell411@gmail.com>
2024-05-29 14:27:56 -0500

    Still in real mode, but added user prompt and the ability to reboot.

--- src/kernel.s
+++ src/kernel.s
@@ -1,10 +1,28 @@
 	mov ah, 0xe
 	mov bx, msg
 	call BIOSprint
+
+	mov bx, menu
+	call BIOSprint
+
+	mov bx, prompt
+	call BIOSprint
+	
+	;menu prompt
+	xor ax, ax
+	int 0x16
+
+	cmp al, "2"
+	jz reboot
 	
 hang:
 	jmp hang	
 
+reboot:
+	int 0x19
+
 	%include "BIOS/biosprint.s"
 
-  	msg db "Something OS", 13, 10, 0
+  	msg db "==========================", 13, 10, 32, 32, 32, 32, 32, 32, 32, "Something OS", 13, 10, "==========================", 13, 10, 0
+	menu db  13, 10, 32, 32, 32, 32, 32, 32, 32, "1 - shell", 13, 10, 32, 32, 32, 32, 32, 32, 32, "2 - reboot", 13, 10, 0
+	prompt db 13, 10, 32, 32, 'sh-$', 32, 0
